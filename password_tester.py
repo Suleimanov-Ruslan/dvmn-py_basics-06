2f35615e8609a2a29edc1123e1450ec749614881
Suleimanov-Ruslan <suleimanovrn@gmail.com>
2025-02-20 14:45:29 +0300

    Update password_tester.py

--- password_tester.py
+++ password_tester.py
@@ -1,29 +1,24 @@
-def is_very_long(password):
-    return len(password) > 12
+def has_digit(password): return any(char.isdigit() for char in password)
 
+def has_upper_letters(password): return any(char.isupper() for char in password)
 
-def has_digit(password):
-    return any(char.isdigit() for char in password)
+def has_lower_letters(password): return any(char.islower() for char in password)
 
+def has_symbols(password): return any(not char.isalnum() for char in password)
 
-def has_upper_letters(password):
-    return any(char.isupper() for char in password)
-
-
-def has_lower_letters(password):
-    return any(char.islower() for char in password)
-
-
-def has_symbols(password):
-    return any(not char.isalnum() for char in password)
-
-
-checks = [is_very_long, has_digit, has_upper_letters, has_lower_letters, has_symbols]
-
-password = input("Введите пароль: ")
+def password_rating(password): checks = [ has_digit, has_upper_letters, has_lower_letters, has_symbols, ]
 
 score = sum(2 for check in checks if check(password))
 
-print("Рейтинг пароля:", score)
+if len(password) > 12:
+    score += 2
+
+return score
+
+def main(): password = input("Введите пароль: ") print(f"Рейтинг пароля: {password_rating(password)}")
+
+if name == "main": main()
+
+
 
 
